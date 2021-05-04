5bea685ceffb08cf7d5bb1456d440e4bc1caa7b4
steve daniels <steve.daniels@ryerson.ca>
2021-05-04 10:04:46 -0400

    add shiftr examples

---
+++ MQTT_SimpleSender_token_ok/arduino_secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+char SHFTR_INSTANCE[] = "public" ; // your SHIFTR instance name --> this = userName in arduino docs
+char SHFTR_TOKEN[] =    "public" ; // your SHIFTR secret key --> this = password in arduino docs
+    // if you leave these as PUBLIC -- view @ this: https://www.shiftr.io/try
+
+char SHIFTR_ID[] = "myTag"; // this is your tag on the shftr graph
